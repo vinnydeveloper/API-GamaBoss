fa4a6701ba611c4ea1f42c8102d607462e796244
StaticDreamstate <lonely_loonie@protonmail.com>
2022-07-27 04:46:18 -0300

    Rota get /

--- src/modules/Cliente/controller.ts
+++ src/modules/Cliente/controller.ts
@@ -7,34 +7,38 @@ import bcrypt from "bcryptjs";
 
 const controller = {
 
-    async createClient(req: Request, res: Response) {
-      const hash = bcrypt.hashSync(req.body.senha, 10);  
-      const { nome, email, senha, telefone, whatsapp } = req.body;
-      const { file } = req;
-    
-        const savedClient = await Client.count({
-          email,
-        });
-    
-        if (savedClient) {
-          logger.warn(`[createClient] Tentativa repetida de cadastro: ${req.socket.remoteAddress}`);
-          return res.status(400).json("Email já cadastrado no banco");
-        }
-    
-        const image = await Image.create({
-          link: `${path.resolve("uploads","images")}${file?.filename}`,
-          nome: file?.filename,
-        });
-    
-        const newClient = await Client.create({
-          ...req.body,
-          senha: hash,
-          images: [image._id],
-        });
-    
-        logger.info(`[createClient] Cliente cadastrado: ${req.socket.remoteAddress}`);
-        return res.status(201).json(newClient);
-      },
+  start(req: Request, res: Response) {
+    return res.status(200).json("API em execução.");
+  },
+
+  async createClient(req: Request, res: Response) {
+    const hash = bcrypt.hashSync(req.body.senha, 10);
+    const { nome, email, senha, telefone, whatsapp } = req.body;
+    const { file } = req;
+
+    const savedClient = await Client.count({
+      email,
+    });
+
+    if (savedClient) {
+      logger.warn(`[createClient] Tentativa repetida de cadastro: ${req.socket.remoteAddress}`);
+      return res.status(400).json("Email já cadastrado no banco");
+    }
+
+    const image = await Image.create({
+      link: `${path.resolve("uploads", "images")}${file?.filename}`,
+      nome: file?.filename,
+    });
+
+    const newClient = await Client.create({
+      ...req.body,
+      senha: hash,
+      images: [image._id],
+    });
+
+    logger.info(`[createClient] Cliente cadastrado: ${req.socket.remoteAddress}`);
+    return res.status(201).json(newClient);
+  },
 
 }
 
