59c35744ce5c46e5122002bf3531fb2d9793b3e9
StaticDreamstate <lonely_loonie@protonmail.com>
2022-07-27 22:32:57 -0300

    Package.json conf

--- src/modules/Auth/routes.ts
+++ src/modules/Auth/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import controller from "./controller";
-import cors from "cors";
 
 const routes = Router();
 
-routes.post("/login", cors(), controller.login);
-routes.post("/reset-senha", cors(), controller.gerarNovoHash);
-routes.post("/recuperar-senha", cors(), controller.recuperarSenha);
+routes.post("/login", controller.login);
+routes.post("/reset-senha", controller.gerarNovoHash);
+routes.post("/recuperar-senha", controller.recuperarSenha);
 
 export default routes;
--- src/modules/Cliente/routes.ts
+++ src/modules/Cliente/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import controller from "./controller";
 import imgUpload from "../../infra/middlewares/imgUpload";
-import cors from "cors"
 
 const routes = Router();
 
 routes.get("/", controller.start);
-routes.post("/cadastro", cors(), imgUpload.single("avatar"), controller.createClient);
+routes.post("/cadastro", imgUpload.single("avatar"), controller.createClient);
 
 export default routes;
